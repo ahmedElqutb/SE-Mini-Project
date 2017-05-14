59876115edee1f5bb69e01fd32b883619202b4ed
ahmedElqutb <ahmedElqutb@users.noreply.github.com>
2017-05-14 22:02:00 +0200

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,6 @@ var portfolioController = require('./controllers/portfolioController');
 var workController = require('./controllers/workController');
 
 // add routes
-router.get('/', userController.welcome);
 
 
 router.get('/register', userController.register);
@@ -26,10 +25,10 @@ router.post('/edit-info', userController.checkAuth, userController.updateInfo);
 router.get('/create-portfolio', userController.checkAuth, portfolioController.createPortfolio);
 router.post('/create-portfolio', userController.checkAuth, portfolioController.storePortfolio);
 
+router.get('/portfolio/:id', portfolioController.viewPortfolio);
 
 router.get('/portfolio', userController.checkAuth, portfolioController.viewMyPortfolio);
 
-router.get('/portfolio/:id', portfolioController.viewPortfolio);
 
 
 router.get('/edit-portfolio', userController.checkAuth, portfolioController.editPortfolio);
@@ -39,21 +38,18 @@ router.post('/edit-portfolio', userController.checkAuth, portfolioController.upd
 router.get('/add-work', userController.checkAuth, workController.addWork);
 router.post('/add-work', userController.checkAuth, workController.storeWork);
 
-
-router.get('/works/:id/remove', userController.checkAuth, workController.removeWork);
-
-router.get('/works/:id', workController.viewWork);
-
-
 router.get('/works/:id/edit', userController.checkAuth, workController.editWork);
 router.post('/works/:id/edit', userController.checkAuth, workController.updateWork);
 
+router.get('/works/:id/remove', userController.checkAuth, workController.removeWork);
 
 router.get('/works/:id1/images/:id2/remove', userController.checkAuth, workController.removeImage);
 
+router.get('/works/:id', workController.viewWork);
+
+router.get('/home/:page', userController.home);
 
 router.get('/home', userController.index);
-router.get('/home/:page', userController.home);
 
 
 router.get('/search/:keyword/:page', userController.searchPaging);
@@ -62,6 +58,9 @@ router.get('/search/:keyword', userController.search);
 
 //logout
 router.get('/logout', userController.checkAuth, userController.logout);
+
+router.get('/', userController.welcome);
+
 
 // export router
 
